f615c8f92ee4b95c30dc8dbbbed8a1fbb47cb63e
RABrL <robin_brito@hotmail.com>
2023-02-05 02:14:52 -0500

    Delete sections FAQs and Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Navbar from './sections/navbar/Navbar'
 import Header from './sections/header/Header'
 import About from './sections/about/About'
-import Services from './sections/services/Services'
 import Portfolio from './sections/portfolio/Portfolio'
-import FAQs from './sections/faqs/FAQs'
 import Contact from './sections/contacts/Contact'
 import Footer from './sections/footer/Footer'
 import FloatingNav from './sections/floating-nav/FloatingNav'
@@ -14,9 +12,7 @@ export default function App () {
       <Navbar/>
       <Header/>
       <About/>
-      <Services/>
       <Portfolio/>
-      <FAQs/>
       <Contact/>
       <Footer/>
       <FloatingNav/>
